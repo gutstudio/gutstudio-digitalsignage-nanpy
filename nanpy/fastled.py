84faaafdd429a5d77826d8dbf5e4cda2f76682d5
Ryan Goh <gohengkeat89@gmail.com>
2018-04-24 23:07:44 +0800

    Initialize FastLED class in py

--- nanpy/fastled.py
+++ nanpy/fastled.py
@@ -4,9 +4,9 @@ from nanpy.arduinoboard import (arduinoobjectmethod, returns)
 class FastLED(ArduinoObject):
     cfg_h_name = 'USE_FastLED'
 
-    def __init__(self, connection=None):
+    def __init__(self, ledType, dataPin, colorOrder, leds, numLEDs, connection=None):
         ArduinoObject.__init__(self, connection=connection)
-        self.id = self.call('new')
+        self.id = self.call('new', ledType, dataPin, colorOrder, leds, numLEDs)
 
     @arduinoobjectmethod
     def show(self):
@@ -23,14 +23,4 @@ class FastLED(ArduinoObject):
     @arduinoobjectmethod
     def setCorrection(self, ledType):
         pass
-
-    class addLeds:
-        def __init__(self, ledType, dataPin, colorOrder):
-            self._LED_TYPE = ledType
-            self._DATA_PIN = dataPin
-            self._COLOR_ORDER = colorOrder
-            
-    @arduinoobjectmethod
-    def customAddLeds(self, addLeds, ledType, dataPin, colorOrder, leds, numLEDs):
-        return addLeds<ledType,dataPin,colorOrder>(leds, numLEDs)
         
